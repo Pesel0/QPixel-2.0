68fd5a72e1104ba81568fb5663a3af085fb0928d
Pesel0 <122075175+Pesel0@users.noreply.github.com>
2023-01-14 16:06:42 +0100

    Delete resources/qpixel-clothes directory

--- resources/qpixel-clothes/client/READDTHESE.lua
+++
@@ -1,5 +0,0 @@
---FOR SPAWN
-
-tatCategory = exports["qpixel-clothes"]:GetTatCategs()
-tattooHashList = exports["qpixel-clothes"]:GetTatCategs()
-Login.custompeds = exports["qpixel-clothes"]:GetCustomSkins()
